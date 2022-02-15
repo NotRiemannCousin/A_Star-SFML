026570ced1046287c712d9fc68726c827b438951
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2022-02-15 08:05:39 -0300

    Update Node.hpp

--- Node.hpp
+++ Node.hpp
@@ -5,66 +5,48 @@ using namespace sf;
 struct Node
 {
   private:
-	Vector2i node, pointer;
+	Vector2i node;
+	Node *pointer;
 	int h = 0, g = 0, f = 0;
 
   public:
-	Node(Vector2i n, Vector2i p, int _h, int _g)
+	Node(Vector2i n, Node &p, int _h, int _g)
+	{
+		pointer = (&p);
+		node = n;
+		h = _h;
+		g = _g;
+		f = g + h;
+	};
+	Node(Vector2i n, Node *p, int _h, int _g)
 	{
 		node = n;
-		pointer = p;
+		pointer = (p);
 		h = _h;
 		g = _g;
 		f = g + h;
 	};
 	Node(){};
-	~Node()
-	{
-		node = Vector2i(0, 0);
-		pointer = Vector2i(0, 0);
-		h = 0;
-		f = 0;
-		g = 0;
-	};
+	~Node(){};
 
 	Vector2i getNode() { return node; };
-	Vector2i getPointer() { return pointer; };
+	Node *getPointer() { return pointer; };
 	int getF() { return f; };
 	int getG() { return g; };
 	int getH() { return h; };
 
-	void update(Vector2i p,int _g)
+	void update(Node &p, int _g)
 	{
-		pointer = p;
+		pointer = &p;
 		g = _g;
 		f = g + h;
 	}
-
-	void restart(Node N)
+	void operator()(Vector2i n, Node *p, int _h, int _g)
 	{
-		f = N.getF();
+		pointer = (p);
+		node = n;
+		h = _h;
+		g = _g;
+		f = g + h;
 	};
-	
-	bool operator>(Node _node)
-	{
-		return f > _node.f;
-	}
-	
-	bool operator<(Node _node)
-	{
-		return f < _node.f;
-	}
-	bool operator==(Node _node)
-	{
-		return (node == _node.node) && (pointer == _node.pointer) && (f == _node.f);
-	}
-
-	void operator=(Node _node)
-	{
-		node = _node.node;
-		pointer = _node.pointer;
-		h = _node.h;
-		f = _node.f;
-		g = _node.g;
-	}
 };
