7647907fe13120936993266eddcc9e6db614bc37
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2022-02-15 08:08:22 -0300

    Update Button.hpp

--- Button.hpp
+++ Button.hpp
@@ -1,63 +1,27 @@
-#include <SFML/Graphics.hpp>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include "Node.hpp"
 
-using namespace sf;
-
-class Button
-
+int dist(Vector2i v1, Vector2i v2)
 {
+	return abs(v1.x - v2.x) + abs(v1.y - v2.y);
+}
 
-  public:
+bool compareColor(Color c1, Color c2)
+{
+	return (c1.r + c1.g + c1.b) > (c2.r + c2.g + c2.b);
+}
 
-	Text text;	RectangleShape box;
-
-	Button(Font &font, char *str, int sizeChar, Vector2f pos, float size) : text(str, font, sizeChar), box(Vector2f(sizeChar * size * 0.912, sizeChar * 1.26))
-
+Node *search(std::vector<Node> &N, Vector2i s)
+{
+	
+	for (int k = 0; k < N.size(); k++)
 	{
-
-		text.setPosition(pos.x - (text.getGlobalBounds().left + text.getGlobalBounds().width / 2 - (sizeChar * 0.912 * size / 2)) - box.getSize().x / 2, pos.y - box.getSize().y / 2);
-
-		text.setFillColor(Color::Black);
-
-		box.setPosition(Vector2f(pos.x - box.getSize().x / 2, pos.y - box.getSize().y / 2));
-
-		box.setFillColor(Color::White);
-
-		box.setOutlineColor(Color(230, 230, 230));
-
-		box.setOutlineThickness(10);
-
-	};
-
-	void click(Touch touch, void action())
-
-	{
-
-		for (int i = 0; i < 5; i++)
-
+		if (N[k].getNode() == s)
 		{
-
-			if (box.getGlobalBounds().contains(Vector2f (touch.getPosition(i))))
-
-			{
-
-				action();
-
-				break;
-
-			}
-
+			return &N[k];
 		}
-
 	}
-
-	void draw(RenderWindow &w)
-
-	{
-
-		w.draw(box);
-
-		w.draw(text);
-
-	};
-
-};
+	return NULL;
+}
